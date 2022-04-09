31bdf350b2c064c565b22ee7a4edcd08e1559682
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2022-04-09 22:14:55 +0530

    did some tweaks

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -34,5 +34,38 @@
         <span class="carousel-control-next-icon"></span>
         </button>
   </div>
+  <div class="trending-wrapper">
+      <h3>Trending Products</h3>
+      <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            @foreach ($products as $item)
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="{{$item['id']}}" class="active" aria-current="true" aria-label="Slide 1"></button>
+          @endforeach
+
+        </div>
+        <div class="carousel-inner">
+            @foreach ($products as $item)
+
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="{{$item['gallery']}}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          @endforeach
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      
+  </div>
 </div>
 @endsection
